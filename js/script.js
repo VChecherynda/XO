c4af5f82d1e8b3dbf2cf5d4336ed1fbf42b4eb5a
Vadym <vadosevich@gmail.com>
2017-01-22 23:06:34 +0200

    Added functions

---
+++ js/script.js
@@ -0,0 +1,90 @@
+class Game {
+  constructor(size){
+    this.size = +size;
+    this.moves = [];
+
+  }
+  
+  prepareField() {
+    var x,y;
+    var field = $('#field');
+    
+    field.empty();
+    
+      for( y = 0; y < this.size; y++) {
+        
+          for( x = 0; x < this.size; x++) {
+            
+            var cell =`<input type="text" data-horizontal="${x}" data-vertical="${y}" class="move-input"></input>`;
+            
+            field.append(cell);  
+          }
+        
+        field.append('<br/>');
+        
+      }
+ 
+  } ;
+
+
+  addMove(move) {
+      
+  };
+
+  isFinished() {
+
+  };
+}
+
+class Move {
+
+  constructor(){
+    this.player = null;
+    this.horizontal = null;
+    this.vertical = null;
+  };
+
+} 
+
+$(document).ready(function(){
+
+  var game; 
+
+  $('#start').click(function(){
+
+    var size = $('#size').val(); 
+    
+    if( isNaN(size) ) {
+
+        return alert('Enter valid size!');
+
+    }
+    
+    game = new Game(size);
+      
+    game.prepareField();
+
+    $('.move-input').click(function(){
+
+      var move = new Move();
+
+      move.player = game.currentPlayer;
+
+      move.vertical = +$(this).data('vertical');
+      move.horizontal = +$(this).data('horizontal'); 
+
+      game.addMove(move);
+      game.isFinished();
+
+    });
+    
+  });
+
+});
+
+
+
+     
+
+
+
