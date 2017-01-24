e7324272cc026a2c75f492135a23d58d71f87fe4
Vadym <vadosevich@gmail.com>
2017-01-25 01:11:00 +0200

    Working alpha

--- js/script.js
+++ js/script.js
@@ -1,44 +1,118 @@
 class Game {
   constructor(size){
     this.size = +size;
-    this.moves = [];
-
+    this.startNewGame();
   }
-  
-  prepareField() {
-    var x,y;
+
+  startNewGame() {
+    this.currentPlayer = 'X';
+    this.moves = new Array(this.size);
+    for(var i = 0; i < this.size; i ++) {
+      this.moves[i] = new Array(this.size);
+    }
+    this.renderFieldHtml();
+    this.addCellClickEventListener();
+  }
+
+  currentPlayerHtml() {
+    return `<div id='game_current_player'>${this.currentPlayer}</div>`;
+  }
+
+  cellHtml(x, y) {
+    return `<td data-vertical="${y}" data-horizontal="${x}" class="move-input" disabled='disabled'></td>`
+  }
+
+  renderFieldHtml() {
     var field = $('#field');
-    
+
     field.empty();
+    field.append(`Current Player is ${this.currentPlayerHtml()}`);
+    field.append('<table>');
+    for(var y = 0; y < this.size; y++) {
+      field.append('<tr>');
+        for(var x = 0; x < this.size; x++) {
+          field.append(this.cellHtml(x,y));
+        }
+      field.append('<tr/>');
+    }
+    field.append('</table>');
+  }
+
+  addCellClickEventListener() {
+    var game = this;
+    $('.move-input').click(function(){
+      var x = +$(this).data('horizontal');  
+      var y = +$(this).data('vertical');
+      $(this).html(game.currentPlayer);
+      game.addMove(x,y);
+    });
+  }
+
+  addMove(x, y) {
+    var move = new Move();
+    move.player = this.currentPlayer;
+    move.horizontal = x; 
+    move.vertical = y;
+    this.moves[y][x] = move;
+    console.log(this.moves);
+    if(this.isFinished(move)) {
+      this.renderWinner();
+      this.startNewGame();
+    }
+
+    this.changeCurrentPlayer();
+  }
+
+  isFinished(move) {
+    var win = true;
     
-      for( y = 0; y < this.size; y++) {
-        
-          for( x = 0; x < this.size; x++) {
-            
-            var cell =`<input type="text" data-horizontal="${x}" data-vertical="${y}" class="move-input"></input>`;
-            
-            field.append(cell);  
-          }
-        
-        field.append('<br/>');
-        
+    // for(var i =0; i < this.size; i++) {
+    //   var subjectCell = this.moves[i][move.horizontal];
+    //   if(!subjectCell || (subjectCell.player != move.player)) {
+    //     win = false
+    //     break;
+    //   }
+    // }
+
+    // for(var i = 0; i < this.size; i++) {
+    //   if(win) break;
+    //   var subjectCell = this.moves[move.vertical][i];
+    //   console.log(subjectCell);
+    //   if(!subjectCell || (subjectCell.player != move.player)) {
+    //     win = false
+    //     break;
+    //   }
+    // }
+
+    // for(var i = 0; i < this.size; i++) {
+    //   //if(win) break;
+    //   var subjectCell = this.moves[i][i];
+    //   console.log(subjectCell);
+    //   if(!subjectCell || (subjectCell.player != move.player)) {
+    //     win = false
+    //     break;
+    //   }
+    // }
+
+    for(var i = 0; i < this.size; i++) {
+      var subjectCell = this.moves[this.size - 1 - i][i];
+      if(!subjectCell || (subjectCell.player != move.player)) {
+        win = false
+        break;
       }
- 
-  };
+    }
 
-  currentPlayer() {
-    var symbol = $(this).val();
-    return symbol;
-  };
 
-  addMove(move) {
-      this.moves.push(move);
-      console.log(this.moves)
-  };
+    return win;
+  }
 
-  isFinished() {
+  changeCurrentPlayer() {
+    this.currentPlayer = (this.currentPlayer == 'X') ? 'O' : 'X'
+  }
 
-  };
+  renderWinner() {
+    alert(`${this.currentPlayer} has won`);
+  }
 }
 
 class Move {
@@ -52,39 +126,12 @@ class Move {
 } 
 
 $(document).ready(function(){
-
-  var game; 
+  var game;
 
   $('#start').click(function(){
-
     var size = $('#size').val(); 
-    
-    if( isNaN(size) ) {
-
-        return alert('Enter valid size!');
-
-    }
-    
+    if( isNaN(size) ) return alert('Enter valid size!');
     game = new Game(size);
-      
-    game.prepareField();
-
-    $('.move-input').keyup(function(){
-
-      var move = new Move();
-
-      move.player = game.currentPlayer();
-
-      move.vertical = +$(this).data('vertical');
-      move.horizontal = +$(this).data('horizontal'); 
-
-      game.addMove(move);
-
-      game.isFinished();
-
-    });
-    
   });
-
 });
 
